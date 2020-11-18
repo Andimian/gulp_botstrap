682947e0a38f16082fdc68578d00a7deaaa88ce5
ÐAnton Sokolov <zvuk45@bk.ru>
2020-11-18 07:31:49 +0500

    first commit

---
+++ src/assets/js/back-to-top.js
@@ -0,0 +1,30 @@
+(function() {
+    'use strict';
+    if(document.querySelector('.back-to-top')) {
+
+    function trackScroll() {
+        var scrolled = window.pageYOffset;
+        var coords = document.documentElement.clientHeight;
+
+        if (scrolled > coords) {
+            goTopBtn.classList.add('back-to-top_show');
+        }
+        if (scrolled < coords) {
+            goTopBtn.classList.remove('back-to-top_show');
+        }
+    }
+
+    function backToTop() {
+        if (window.pageYOffset > 0) {
+            window.scrollBy(0, -80);
+            setTimeout(backToTop, 0);
+        }
+    }
+
+    var goTopBtn = document.querySelector('.back-to-top');
+
+    window.addEventListener('scroll', trackScroll);
+    goTopBtn.addEventListener('click', backToTop);
+    }
+
+})();
